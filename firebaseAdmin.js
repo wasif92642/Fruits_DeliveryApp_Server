3bdb23d829521a27182a2bec78523526cfaf29d4
Wasif Ali Kazmi <88131946+wasif92642@users.noreply.github.com>
2024-06-03 09:50:29 +0500

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -2,8 +2,8 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./secrects.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://vita-319b7-default-rtdb.firebaseio.com"
+  credential: admin.credential.cert(),
+  databaseURL: ""
 
 });
 
